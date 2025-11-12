f676601cbb9ea986dd037b1a7f0d2c5408dcd848
zythedeveloper <zythedeveloper@gmail.com>
2025-11-12 10:22:45 +0800

    Add Resume Capability and Skip Counter to `video_downloader.py`

--- start_kit/video_downloader.py
+++ start_kit/video_downloader.py
@@ -1,10 +1,8 @@
-import os, json, time, sys, random, logging
+import os, json, time, sys, random, logging, glob
 import urllib.request
+import pandas as pd
 from multiprocessing.dummy import Pool
 from concurrent.futures import ThreadPoolExecutor, as_completed
-
-logging.basicConfig(filename='download_{}.log'.format(int(time.time())), filemode='w', level=logging.DEBUG)
-logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 # Set this to youtube-dl if you want to use youtube-dl.
 # The the README for an explanation regarding yt-dlp vs youtube-dl.
@@ -113,24 +111,66 @@ def process_video(gloss, video_url, video_id, saveto):
         download_yt_videos(saveto, video_url)
 
 
+def get_latest_log(log_dir):
+    log_files = glob.glob(os.path.join(log_dir, "*.log"))
+    if not log_files:
+        return None
+    latest_log = max(log_files, key=os.path.getmtime)
+
+    return latest_log
+
+
+def get_skip_counter(indexfile, logfile):
+    skip_counter = 0
+    df = pd.read_json(indexfile)
+    df_exploded = df.explode("instances", ignore_index=True)
+    df_flat = pd.concat([df_exploded.drop(columns=["instances"]),pd.json_normalize(df_exploded["instances"])],axis=1)
+
+    # read the log file and get the index of latest video_id
+    with open(logfile, "r") as f:
+        try:
+            lines = f.readlines()
+
+            log_df = pd.DataFrame({"raw": [line.strip() for line in lines]})
+            log_df["video_id"] = log_df["raw"].str.extract(r"video:?\s*(\d+)")
+            index = df_flat[df_flat["video_id"] == log_df['video_id'].dropna().iloc[-1]].index[0]
+
+            if index > 0:
+                skip_counter = index
+
+        except Exception as e:
+                logging.error(f"Error: {e}")
+
+    return skip_counter
+
+
 if __name__ == '__main__':
     indexfile = 'WLASL_v0.3.json'
     saveto = 'raw_videos'
+    
     os.makedirs(saveto, exist_ok=True)
-
     content = json.load(open(indexfile))
+    LOGFILE = get_latest_log(os.getcwd())
+
+    logging.basicConfig(filename='download_{}.log'.format(int(time.time())), filemode='w', level=logging.DEBUG)
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
     logging.info('Start downloading videos using multithreading.')
 
-    MAX_WORKERS = 8
+    MAX_WORKERS = os.cpu_count() - 4
     futures = []
+    count = 0
+    skip_counter = get_skip_counter(indexfile, LOGFILE) - (MAX_WORKERS*2)
 
     with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
         for entry in content:
             gloss = entry['gloss']
             for inst in entry['instances']:
-                video_url = inst['url']
-                video_id = inst['video_id']
-                futures.append(executor.submit(process_video, gloss, video_url, video_id, saveto))
+                if count >= skip_counter:
+                    video_url = inst['url']
+                    video_id = inst['video_id']
+                    futures.append(executor.submit(process_video, gloss, video_url, video_id, saveto))
+
+                count += 1
 
         for future in as_completed(futures):
             try:
